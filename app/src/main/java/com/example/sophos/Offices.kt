95e06e0227b794372fab9768aede548c13a54c32
Juan Diego Salazar Velasquez <salazar.jd@gmail.com>
2022-12-23 11:19:26 -0500

    oficinas marcadas en mapa

--- app/src/main/java/com/example/sophos/Offices.kt
+++ app/src/main/java/com/example/sophos/Offices.kt
@@ -1,17 +1,27 @@
 package com.example.sophos
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class Offices : AppCompatActivity(), OnMapReadyCallback {
+class Offices : AppCompatActivity(), OnMapReadyCallback, OnMyLocationButtonClickListener {
 
-    private lateinit var map:GoogleMap
+    private lateinit var map: GoogleMap
+
+    companion object{
+        const val REQUEST_CODE_LOCATION = 0
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +29,7 @@ class Offices : AppCompatActivity(), OnMapReadyCallback {
         createFragment()
     }
 
-    private fun createFragment(){
+    private fun createFragment() {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
@@ -27,9 +37,11 @@ class Offices : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
         createMarker()
+        map.setOnMyLocationButtonClickListener(this)
+        enableLocation()
     }
 
-    private fun createMarker(){
+    private fun createMarker() {
         val coordinates = LatLng(6.218229100000025, -75.58021739999998)
         val coordinates2 = LatLng(6.224464299999994, -75.57474939999997)
         val marker = MarkerOptions().position(coordinates).title("CEOH - 107")
@@ -41,5 +53,54 @@ class Offices : AppCompatActivity(), OnMapReadyCallback {
             4000,
             null
         )
+    }
+
+    private fun isLocationPermissionGranted() = ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun enableLocation(){
+        if(!::map.isInitialized) return
+        if(isLocationPermissionGranted()){
+            map.isMyLocationEnabled = true
+        }else{
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+            Toast.makeText(this, "Ve a ajustes y acepta los persmisos", Toast.LENGTH_SHORT).show()
+        }else{
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            REQUEST_CODE_LOCATION -> if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                map.isMyLocationEnabled = true
+            }else{
+                Toast.makeText(this, "Ve a ajustes y acepta los persmisos", Toast.LENGTH_SHORT).show()
+            }
+            else -> {}
+        }
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        if(!::map.isInitialized) return
+        if(!isLocationPermissionGranted()){
+            map.isMyLocationEnabled = false
+        }
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        return false
     }
 }
